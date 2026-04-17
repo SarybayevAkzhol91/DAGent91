c453382c078c5b37c9048b3e108647ebb0c2c24b
SarybayevAkzhol91 <agroqazyna@gmail.com>
2026-04-17 18:24:42 +0500

    Create setup.py

---
+++ dagent/dagent/dagent/dagent/dagent/dagent/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="dagent",
+    version="0.1.0",
+    author="СарьбаевАкжол91",
+    description="DAG-based Multi-Agent System",
+    packages=find_packages(),
+    python_requires=">=3.8",
+)
